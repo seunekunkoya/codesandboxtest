6bea2dbcf4c4822ef1053a772f3a45687b674466
ekunkoya.io <50266941+seunekunkoya@users.noreply.github.com>
2021-02-04 08:04:07 +0100

    new change

--- src/index.js
+++ src/index.js
@@ -26,17 +26,3 @@ mongoose.connection.on("error", (err) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/images", express.static(path.join("images")));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, PUT, OPTIONS"
-  );
-  next();
-});
-app.use(cors());
